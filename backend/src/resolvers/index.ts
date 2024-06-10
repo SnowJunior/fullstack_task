fd89457efc26ba798eb2bdc7e988a268e29262ef
Snow <onyangogeorge954@gmail.com>
2024-06-10 13:44:53 +0300

    task: create mutations for add and deleting books to and from the reading list

--- backend/src/resolvers/index.ts
+++ backend/src/resolvers/index.ts
@@ -1,7 +1,26 @@
 import { booksData } from '../data/books';
+import { readingList } from '../data/teacher';
+import { Book } from '../model/book';
 
 export const resolvers = {
   Query: {
     books: () => booksData,
+    readingList: () => readingList,
+  },
+  Mutation: {
+    addBookToList: (_: never, { author }: {author: string}): Book[] => {
+      const book = booksData.find((b) => b.author === author);
+      if (book && !readingList.find((b) => b.author === author)) {
+        readingList.push(book);
+      }
+      return readingList;
+    },
+    removeBookFromList: (_: never,{ author }: {author: string}): Book[] => {
+      const index = readingList.findIndex((b) => b.author === author);
+      if (index !== -1) {
+        readingList.splice(index, 1);
+      }
+      return readingList;
+    },
   },
 };
